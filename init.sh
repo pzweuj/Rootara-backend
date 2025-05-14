859c9a44c6bd44def97ff5865749fc1d18627e81
EuJ <pzweuj@live.com>
2025-05-14 16:44:19 +0800

    v0.4.1
    更新初始化脚本

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 # 初始化
 ## 数据库初始化
-python /app/scripts/rootara_initial.py --name $USERNAME --email $EMAIL --db /data/rootara.db --force False
+user=admin
+email=admin@rootara.app
+python /app/scripts/rootara_initial.py --name "${USERNAME:-$user}" --email "${EMAIL:-$email}" --db /data/rootara.db --force False
 
 # FASTAPI启动
 python /app/main.py
